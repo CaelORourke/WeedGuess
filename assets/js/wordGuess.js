9034843341b24313ea49a0ca62b01c245ac00a3b
Shane <shanegates@hotmail.com>
2019-05-04 15:22:48 -0700

    moved letterScores into wordGuess

--- assets/js/wordGuess.js
+++ assets/js/wordGuess.js
@@ -11,6 +11,35 @@ const wordGuess = (function () {
     let score = 0;
     let wins = 0;
 
+    const letterScores = {
+        'a': 1,
+        'b': 3,
+        'C': 3,
+        'd': 2,
+        'e': 1,
+        'f': 4,
+        'g': 2,
+        'h': 4,
+        'i': 1,
+        'j': 8,
+        'k': 5,
+        'l': 1,
+        'm': 3,
+        'n': 1,
+        'o': 1,
+        'p': 3,
+        'q': 10,
+        'r': 1,
+        's': 1,
+        't': 1,
+        'u': 1,
+        'v': 4,
+        'w': 4,
+        'x': 8,
+        'y': 4,
+        'z': 10
+        };
+
     function chooseRandomWord() {
         currentWord = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
         console.log(`currentWord='${currentWord}'`);
